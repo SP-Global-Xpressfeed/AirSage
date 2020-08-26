ff2ba3f7ace34f746daaf7f50c2eefd62fc77156
PythonAuotmation <68327511+PythonAuotmation@users.noreply.github.com>
2020-08-26 21:26:04 +0530

    updated

--- MSSQL/Top_20_Census_Tracts_That_Generated_Traffic.sql
+++ MSSQL/Top_20_Census_Tracts_That_Generated_Traffic.sql
@@ -1,5 +1,5 @@
 /***********************************************************************************************
-Top 20 Census Tracts That Generated Traffic To The Santa Rosa Plaza
+Top 20 Census Tracts That Generated Traffic To The Santa Rosa Plaza 
 
 Packages Required:
 Airsage 2017
@@ -26,21 +26,21 @@ Query_Added_Date:
 DatasetKey:
 35
 
-This sample query returns the top 20 census tracts that generated traffic to the Santa Rosa Plaza for June, 2018.
+This sample query returns the top 20 census tracts that generated traffic to the Santa Rosa Plaza for June, 2018
 
 ***********************************************************************************************/
 
 select top 20 p.propertyid, p.propertyName, censusTractHome , cy.countyName ,st.name as stateName , totalSightingsMH
 from snlProp p
 join airsageMonthlyHome mh
-	on p.propertyId = mh.propertyId
+        on p.propertyId = mh.propertyId
 join refUSCensusTract ct
-	on mh.censusTractHome = ct.censusTractFips
+        on mh.censusTractHome = ct.censusTractFips
 join refUSCensusCounty cy
-	on ct.CountyFips = cy.countyFIPS
+        on ct.CountyFips = cy.countyFIPS
 join refUSCensusState st
-	on ct.stateFIPS = st.stateFIPS
-where p.propertyId = 31713	--Santa Rosa Plaza
+        on ct.stateFIPS = st.stateFIPS
+where p.propertyId = 31713      --Santa Rosa Plaza
 and mh.AsOfDate = '6/30/2018'
-and mh.timeOfDay = 8		--All Day
+and mh.timeOfDay = 8            --All Day
 order by totalSightingsMH desc
