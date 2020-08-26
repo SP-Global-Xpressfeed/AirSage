a83e35ac4a03f754e047a9baf2b80f0804d3a245
PythonAuotmation <68327511+PythonAuotmation@users.noreply.github.com>
2020-08-26 21:26:01 +0530

    updated

--- MSSQL/Number_Of_Visitors_To_Park_Meadows_Mall.sql
+++ MSSQL/Number_Of_Visitors_To_Park_Meadows_Mall.sql
@@ -23,14 +23,14 @@ Query_Added_Date:
 DatasetKey:
 35
 
-This sample query returns the number of visitors to Park Meadows Mall, Centennial, Colorado on Black Friday for each of the last three years (2017,2018,2019), and how many of those visitors are determined to have come from higher income groups.
+This sample query returns the number of visitors to Park Meadows Mall, Centennial, Colorado on Black Friday for each of the last three years (2017,2018,2019), and how many of those visitors are determined to have come from higher income groups
 
 ***********************************************************************************************/
 
 select p.propertyid, p.propertyName, ds.asOfDate, ds.totalDevicesDS, ds.income75To100KDS, ds.income100To125KDS, ds.income125To150KDS, ds.income150To200KDS, ds.income200KPlusDS
 from dbo.snlProp p
 join airsageDailySummary ds
-	on p.propertyId = ds.propertyId
-where p.propertyId = 36163		--Park Meadows Mall
-and ds.AsOfDate in ('11/24/2017','11/23/2018','11/29/2019')	--Black Friday
+        on p.propertyId = ds.propertyId
+where p.propertyId = 36163              --Park Meadows Mall
+and ds.AsOfDate in ('11/24/2017','11/23/2018','11/29/2019')     --Black Friday
 order by p.propertyName, ds.asOfDate
