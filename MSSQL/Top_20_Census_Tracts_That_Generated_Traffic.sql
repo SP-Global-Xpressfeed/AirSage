2d9a6b33f0dffd4ec5a1fe75b79dd105f766e2c8
DipsikhaSarkar <65063520+DipsikhaSarkar@users.noreply.github.com>
2020-08-26 17:14:00 +0530

    Update Top_20_Census_Tracts_That_Generated_Traffic.sql

--- MSSQL/Top_20_Census_Tracts_That_Generated_Traffic.sql
+++ MSSQL/Top_20_Census_Tracts_That_Generated_Traffic.sql
@@ -2,11 +2,17 @@
 Top 20 Census Tracts That Generated Traffic To The Santa Rosa Plaza
 
 Packages Required:
+Airsage 2017
 
 Universal Identifiers:
 propertyId
 
 Primary Columns Used:
+asOfDate
+censusTractHome
+countyFIPS
+propertyId
+timeOfDay
 
 Database_Type:
 MSSQL
